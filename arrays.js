e8001069e7f174e9c2d7693a1a421fc20cc5d4dc
Yael Marmon <ymarmon@gmail.com>
2017-11-02 21:15:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,15 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(0)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+ array.pop()
+ return array
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
