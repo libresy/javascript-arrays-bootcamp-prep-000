cc51971233aec846af5e9b85e6454f07ebad17b9
Yael Marmon <ymarmon@gmail.com>
2017-11-03 02:37:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
+
+function removeElementFromEndOfArray(array){
+    return array.slice(0, array.length - 1)
+}
